2400b32a8ab1f5c115147a3408833dd3b1bffd12
Jean Carlo Monge Barboza <jeancarlo2101@gmail.com>
2021-05-19 18:13:02 -0600

    Fixing admin unit tests.

--- apps/admin/src/app/app.component.ts
+++ apps/admin/src/app/app.component.ts
@@ -8,6 +8,8 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  title = 'Welcome to Admin Application!';
   hello$ = this.http.get<Message>('/api');
+
   constructor(private http: HttpClient) {}
 }
